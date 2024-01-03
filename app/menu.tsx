38c6c2a3165e4d68c3d7fac5b1abbde6e33dd476
Bryant Omoregie <unclebryant@gmail.com>
2024-01-03 07:44:00 -0600

    menu input

--- app/menu.tsx
+++ app/menu.tsx
@@ -4,12 +4,18 @@ import { MdKeyboardDoubleArrowRight, MdOutlineChecklist } from "react-icons/md";
 
 export default function Menu() {
   return (
-    <div className="bg-slate-50 h-screen rounded-lg">
+    <div className="bg-slate-50 h-screen rounded-lg w-1/5 p-4">
       <div>
-        <header className="font-header ">Menu</header>
-        <div>
-          <HiOutlineMagnifyingGlass />
-          <text>search</text>
+        <header className="font-header pb-4">Menu</header>
+        <div className="relative">
+          <div className="absolute top-2 left-1">
+            <HiOutlineMagnifyingGlass />
+          </div>
+          <input
+            type="search"
+            placeholder="search"
+            className="bg-slate-50 w-full border border-solid border-slate-400 flex flex-row items-center rounded-md p-1 pl-6 placeholder:text-slate-500"
+          ></input>
         </div>
         <header>Tasks</header>
         <div>
